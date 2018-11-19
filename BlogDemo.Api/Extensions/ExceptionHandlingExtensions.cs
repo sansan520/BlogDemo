a1bc4455fe72c9cc24940a62cb13d70a48dba15c
王槐三 <632494788@qq.com>
2018-11-19 11:33:04 +0800

    Exception 错误处理

---
+++ BlogDemo.Api/Extensions/ExceptionHandlingExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace BlogDemo.Api.Extensions
+{
+   /// <summary>
+   /// 自定义扩展类
+   /// </summary>
+    public static class ExceptionHandlingExtensions
+    {
+        public static void UseHuaisanExceptionHandler(this IApplicationBuilder app,ILoggerFactory loggerFactory)
+        {
+            app.UseExceptionHandler(options=> {
+                options.Run(async context=> {
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var ex = context.Features.Get<IExceptionHandlerFeature>();
+                    if (ex != null)
+                    {
+                        var logger = loggerFactory.CreateLogger("BlogDemo.Api.Extensions.ExceptionHandlingExtensions");
+                        logger.LogError(500, ex.Error, ex.Error.Message);
+                    }
+
+                    await context.Response.WriteAsync(ex?.Error?.Message ?? "An Error Occurred.");
+                });
+            });
+        }
+    }
+}
--- BlogDemo.Api/StartupDevelopment.cs
+++ BlogDemo.Api/StartupDevelopment.cs
@@ -13,11 +13,20 @@ using Blog.Core.Repositories;
 using Blog.Infrastructure.Repositories;
 using Blog.Core;
 using Blog.Infrastructure;
+using BlogDemo.Api.Extensions;
+using Microsoft.Extensions.Logging;
 
 namespace BlogDemo.Api
 {
     public class StartupDevelopment
     {
+        public static IConfiguration _configuration { get; set; }
+
+        public StartupDevelopment(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -29,19 +38,30 @@ namespace BlogDemo.Api
             });
             services.AddMvc();
 
-            services.AddDbContext<MyContext>(options=> {
-                options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=BlogDemo;Trusted_Connection=True;MultipleActiveResultSets=true");
-            });
+            #region 读取json配置文件
+            //services.AddDbContext<MyContext>(options=> {
+            //    options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=BlogDemo;Trusted_Connection=True;MultipleActiveResultSets=true");
+            //});
+            //方法一：
+            //var connectionString = _configuration["ConnectionStrings:DefaultConnection"];
+            //方法二：
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<MyContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            }); 
 
+            #endregion
             services.AddScoped<IPostRepository, PostRepository>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app,ILoggerFactory loggerFactory)
         {
             // 管道先后顺序需要注意
-            app.UseDeveloperExceptionPage();
+            //app.UseDeveloperExceptionPage();//为开发人员启用的错误页面，方便查看错误以及调试，正式环境可取消<-> webapi 可用也可不用，webapi 可返回自定义的错误码等
+            app.UseHuaisanExceptionHandler(loggerFactory);
             app.UseHttpsRedirection();// https
             app.UseMvc();
         }
